ca8e6fa794314ecc27d45b8c48b660fe7198209b
Tushar Gajwani <89583890+TusharG7@users.noreply.github.com>
2023-05-27 16:54:54 +0530

    1 task done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "tailwindcss/tailwind.css";
 
+import { Provider } from 'react-redux';
+
+import {store} from './redux/store'
+
 console.log("hi");
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>, 
   document.getElementById("root")
 );
